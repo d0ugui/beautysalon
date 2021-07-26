da8e0a764375c432d12944dd9588cdaed2de39e2
Douglas Oliveira <douglaspo_97@outlook.com>
2021-07-26 15:19:56 -0300

    Conclusão Aula 03

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+/* Testiomonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal - mostrar elementos ao scrollar a página*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  durantion: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
