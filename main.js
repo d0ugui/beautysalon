b49fe0f9d7622e78e59e4123a4ed1c4482f3bbaa
Douglas Oliveira <douglaspo_97@outlook.com>
2021-07-26 20:10:08 -0300

    Conclusão Aula 04

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for (const link of links) {
 }
 
 //** Ativando a sombra no Header da página quando scrollar */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testiomonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -53,7 +53,25 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Back-to-top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll()
+  backToTop()
+})
